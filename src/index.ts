828ea142e6aa0788f584be2fe550cd50da020b99
Kyle Peacock <kylpeacock@gmail.com>
2022-08-22 16:22:16 -0700

    compatibility example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { HttpAgent } from '@dfinity/agent';
 import NFT from './standard_wrappers/nft_standards/default';
 import EXT from './standard_wrappers/nft_standards/ext';
 import ICPunks from './standard_wrappers/nft_standards/ic_punks';
 
 export * from './registries';
-export *  from './interfaces/nft';
-export *  from './interfaces/token';
+export * from './interfaces/nft';
+export * from './interfaces/token';
 export * as NFTInterfaces from './interfaces/nft';
-export * as TokenInterfaces from './interfaces/token'
+export * as TokenInterfaces from './interfaces/token';
 export { default as standards } from './constants/standards';
+export const LegacyHttpAgent = HttpAgent;
